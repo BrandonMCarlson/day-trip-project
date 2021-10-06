683c9f2b5e595ffac24f621b92f45570278c29ff
Brandon Carlson <Brandoncarlson93bc@gmail.com>
2021-10-05 17:42:22 -0700

    Added 4 Array types for each category required in the random selection, also I have added 4 functions with just their titles; will be filling the functions with more code to complete actions

--- DayTripper.js
+++ DayTripper.js
@@ -1,1 +1,40 @@
-console.log("Hello World");
+"Use Strict"; 
+
+// day trip project for our first project
+// create day trip planner that will randomly choose a destination/restaurant/mode of travel/extra activity
+
+let destinations = ['Las Vegas', 'Chicago', 'New York', 'Seattle']
+let restaurant = ['Steakhouse', 'Seafood', 'Asian cuisine', 'Mexican']
+let travelMode = ['Car', 'Plane', 'Train',]
+let extraActivity = ['Live Performance', 'Casino', 'Scenic view', 'Tour']
+
+function getPlace[]{
+    for (let i = 0; i < destinations.random; i++) {
+        return false;
+    }
+}
+
+function getFood[]
+
+
+
+
+
+
+
+
+
+
+function getTravel[]
+
+
+
+
+
+
+
+
+
+
+
+function getActivity[]
