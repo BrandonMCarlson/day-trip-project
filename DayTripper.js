f5049923e65dd56e17575c29ea7a4a5406a627ca
BrandonMCarlson <Brandoncarlson93bc@gmail.com>
2021-10-06 16:38:50 -0700

    Updated file with correct while statement. This code now ask the User if they are okay with the selections and if not it will re run the program to get different results; now working on cleaning up the code and refactoring.

--- DayTripper.js
+++ DayTripper.js
@@ -13,29 +13,31 @@ let randomFood = restaurant[Math.floor(Math.random() * restaurant.length)];
 let randomTravel = travelMode[Math.floor(Math.random() * travelMode.length)];
 let randomActivity = extraActivity[Math.floor(Math.random() * extraActivity.length)];
 
+// prompt with user input per each random selection until user is satisfied.
+
 let results = (`${randomDest} is your destination. You will be eating at a ${randomFood} restaurant. You will be traveling via a ${randomTravel}. After your dinner you can enjoy ${randomActivity}`); 
-console.log(results);
-// function getFood[]
+
+alert(results); 
+
+let isUserFinished = false; 
+
+while(isUserFinished === false){
+    let userInput = prompt("Is this okay for your trip?");
+    if (userInput === "yes"){
+        isUserFinished = true;
+    }else if (userInput === "no") {
+         randomDest = destinations[Math.floor(Math.random() * destinations.length)];
+         randomFood = restaurant[Math.floor(Math.random() * restaurant.length)];
+         randomTravel = travelMode[Math.floor(Math.random() * travelMode.length)];
+         randomActivity = extraActivity[Math.floor(Math.random() * extraActivity.length)];
+    }
+    alert(results);
+} 
+
+console.log(results); 
+
+
+//prompt inside a while loop
 
 
 
-
-
-
-
-
-
-
-// function getTravel[]
-
-
-
-
-
-
-
-
-
-
-
-// function getActivity[]
